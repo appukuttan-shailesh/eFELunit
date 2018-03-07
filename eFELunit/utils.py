3f74708bb87fd920d8b9aad366f9b4c8ffaca371
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2018-03-07 17:00:57 +0100

    update model loader utility

---
+++ eFELunit/utils.py
@@ -0,0 +1,138 @@
+"""
+Module for loading BluePyOpt optimized model files
+"""
+
+import os
+import sciunit
+from neuronunit.capabilities import ReceivesSquareCurrent, ProducesMembranePotential, Runnable
+from neuron import h, hclass
+import neo
+from quantities import ms
+
+import zipfile
+import json
+import collections
+
+class CellModel(sciunit.Model,
+                         ReceivesSquareCurrent,
+                         ProducesMembranePotential,
+                         Runnable):
+    def __init__(self, model_path=None, model_name=None):
+        # `model_path` is the path to the model's directory (.zip inside this directory)
+        if not os.path.isdir(model_path):
+            raise IOError("Invalid model path: {}".format(model_path))
+
+        if not model_name:
+            file_name = os.path.basename(model_path)
+            model_name = file_name.split(".")[0]
+
+        self.model_name = model_name
+        self.base_path = os.path.join(model_path, self.model_name)
+        self.owd = os.getcwd()     # original working directory saved to return later
+
+        file_ref = zipfile.ZipFile(self.base_path+".zip", 'r')
+        file_ref.extractall(model_path)
+        file_ref.close()
+
+        self.load_mod_files()
+        self.load_cell_hoc()
+
+        # get model template name
+        # could also do this via other JSON, but morph.json seems dedicated for template info
+        with open(os.path.join(self.base_path, "config", "morph.json")) as morph_file:
+            model_template = json.load(morph_file, object_pairs_hook=collections.OrderedDict).keys()[0]
+
+        # access model config info
+        with open(os.path.join(self.base_path, "config", "parameters.json")) as params_file:
+            params_data = json.load(params_file, object_pairs_hook=collections.OrderedDict)
+
+        # extract v_init and celsius (if available)
+        try:
+            h.v_init = None
+            h.celsius = None
+            for item in params_data[model_template]["fixed"]["global"]:
+                # would have been better if info was stored inside a dict (rather than a list)
+                if "v_init" in item:
+                    item.remove("v_init")
+                    h.v_init = float(item[0])
+                if "celsius" in item:
+                    item.remove("celsius")
+                    h.celsius = float(item[0])
+        else:
+            if h.v_init == None:
+                h.v_init = -70.0
+                print("Could not find model specific info for `v_init`; using default value of {} mV".format(str(h.v_init)))
+            if h.celsius == None:
+                h.celsius = 34.0
+                print("Could not find model specific info for `celsius`; using default value of {} degrees Celsius".format(str(h.celsius))
+        """
+        self.id = id
+        self.version = version
+        """
+        self.cell = getattr(h, model_template)(os.path.join(self.base_path, "morphology"))
+        self.iclamp = h.IClamp(0.5, sec=self.cell.soma[0])
+        self.vm = h.Vector()
+        self.vm.record(self.cell.soma[0](0.5)._ref_v)
+        sciunit.Model.__init__(self, name=model_name)
+
+    def load_mod_files(self):
+        os.chdir(self.base_path)
+        libpath = "x86_64/.libs/libnrnmech.so.0"
+        os.system("nrnivmodl mechanisms")   # do nrnivmodl in mechanisms directory
+        if not os.path.isfile(os.path.join(self.base_path, libpath)):
+            raise IOError("Error in compiling mod files!")
+        h.nrn_load_dll(libpath)
+        os.chdir(self.owd)
+
+    def load_cell_hoc(self):
+        with open(os.path.join(self.base_path, "..", self.model_name+'_meta.json')) as meta_file:
+            meta_data = json.load(meta_file, object_pairs_hook=collections.OrderedDict)
+        best_cell = meta_data["best_cell"]
+        self.hocpath = os.path.join(self.base_path,"checkpoints",str(best_cell))
+
+        if os.path.exists(self.hocpath):
+            print("Model = {}: using (best cell) {}".format(self.model_name,best_cell))
+        else:
+            self.hocpath = None
+            for filename in os.listdir(os.path.join(self.base_path, "checkpoints")):
+                if filename.startswith("cell") and filename.endswith(".hoc"):
+                    self.hocpath = os.path.join(self.base_path, "checkpoints", filename)
+                    print("Model = {}: cell.hoc not found in /checkpoints; using {}".format(self.model_name,filename))
+                    break
+            if not os.path.exists(self.hocpath):
+                raise IOError("No appropriate .hoc file found in /checkpoints")
+
+    def get_membrane_potential(self):
+        """Must return a neo.AnalogSignal."""
+        signal = neo.AnalogSignal(self.vm,
+                                  units="mV",
+                                  sampling_period=h.dt * ms)
+        return signal
+
+    def inject_current(self, current):
+        """
+        Injects somatic current into the model.
+
+        Parameters
+        ----------
+        current : a dictionary like:
+                      {'amplitude':-10.0*pq.pA,
+                       'delay':100*pq.ms,
+                       'duration':500*pq.ms}}
+                  where 'pq' is the quantities package
+        """
+        self.iclamp.amp = current["amplitude"]
+        self.iclamp.delay = current["delay"]
+        self.iclamp.dur = current["duration"]
+
+    def run(self, tstop):
+        t_alert = 100.0
+        h.check_simulator()
+        h.cvode.active(0)
+        self.vm.resize(0)
+        h.finitialize(h.v_init)
+        while h.t < tstop:
+            h.fadvance()
+            if h.t > t_alert:
+                print("Time: {} ms".format(t_alert))
+                t_alert += 100.0
