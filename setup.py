fe9e9d0f9ab342b10e157934f04ba8f4cb4065d2
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2019-02-15 17:59:34 +0100

    Update to v1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='eFELunit',
-    version='1.0',
+    version='1.1',
     author='Shailesh Appukuttan, Andrew Davison',
     author_email='shailesh.appukuttan@unic.cnrs-gif.fr, andrew.davison@unic.cnrs-gif.fr',
     packages=['eFELunit',
